901bc6b9cb3fd247525cceb7dd5bd10a0105a99b
snoeyz <108549238+snoeyz@users.noreply.github.com>
2024-07-21 12:46:43 -0500

    Add animation to loading screen

--- scripts/base_loading_scene.gd
+++ scripts/base_loading_scene.gd
@@ -1,5 +1,13 @@
 class_name BaseLoadingScene extends Control
 # Loading scene only exists to either unload resources or keep resources loaded
 
+var anim_timer: float = 0.0
+
+func _process(delta: float) -> void:
+	anim_timer += delta
+	if anim_timer > 0.33:
+		anim_timer = 0.0
+		$Label.text += "."
+
 func _on_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
