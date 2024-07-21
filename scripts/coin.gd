c7e28f9c05ce18825ae006ca97648cdb03c1653d
snoeyz <108549238+snoeyz@users.noreply.github.com>
2024-07-21 01:12:19 -0500

    Add RefCounted to HUD

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,7 +1,11 @@
 class_name Coin extends Area2D
 
+var player_inventory: PlayerInventory
+
+func _ready() -> void:
+	player_inventory = ResourceLoader.load("res://data/player_inventory.tres")
+
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		var inventory: PlayerInventory = ResourceLoader.load("res://data/player_inventory.tres")
-		inventory.coin_count += 1
+		player_inventory.coin_count += 1
 		queue_free()
--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,12 +1,27 @@
 class_name HUD extends CanvasLayer
 
 var player_inventory: PlayerInventory
-@onready var coin_label: Label = $MarginContainer/PanelContainer/MarginContainer/CoinLabel
+
+@onready var ref_counted_label: Label = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/RefCountedLabel
+@onready var coin_label: Label = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/CoinLabel
 
 func _ready() -> void:
 	player_inventory = ResourceLoader.load("res://data/player_inventory.tres")
 	update_coin_count()
 	player_inventory.changed.connect(update_coin_count)
 
+func _process(_delta: float) -> void:
+	update_reference_count()
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_text_indent"):
+		player_inventory.unreference()
+		update_reference_count()
+	elif Input.is_action_just_pressed("ui_cancel"):
+		get_tree().reload_current_scene()
+
 func update_coin_count() -> void:
 	coin_label.text = "Coins Collected: %d" % player_inventory.coin_count
+
+func update_reference_count() -> void:
+	ref_counted_label.text = "References to PlayerInventory: %d" % player_inventory.get_reference_count()
