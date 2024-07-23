052e27faa90f9ab793d7f6b6f3575f7e755c7bfb
snoeyz <108549238+snoeyz@users.noreply.github.com>
2024-07-23 02:03:13 -0500

    Remove start menu

--- scripts/start_game_menu.gd
+++
@@ -1,7 +0,0 @@
-extends CanvasLayer
-
-@onready var start_game_button: Button = $StartGameButton
-
-
-func _on_start_game_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/world.tscn")
